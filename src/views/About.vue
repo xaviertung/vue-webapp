e69c07f5dd625b8abc867ee70938b729952d33b7
xaviertung <xaviertung@yeah.net>
2018-12-29 01:33:48 +0800

    feat: add calender

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      <!-- <Calender /> -->
+    </div>
   </div>
 </template>
+<script>
+// import Calender from '@/components/Calender.vue';
+
+export default {
+  name: 'home',
+  components: {
+    // Calender,
+  },
+};
+</script>
